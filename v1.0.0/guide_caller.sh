d53b04661a09c747c6a839321a2fead7e9e28e26
yuki noguchi <131225436+nyuhki@users.noreply.github.com>
2024-01-26 12:16:02 +0900

    Add files via upload

--- v1.0.0/guide_caller.sh
+++ v1.0.0/guide_caller.sh
@@ -1,109 +1,136 @@
 #!/usr/sh
 
-##########################################################################################
+###########################################################################
+#guide_caller_v1.0.0/guide_caller.sh
+#
+#	 Copyright (c) 2024, Noguchi Yuki (Jun Suzuki lab)
+#	 This software is released under the MIT License, see LICENSE (https://opensource.org/license/mit/).
+#    @citation: Noguchi, Y., Onodera, Y., Maruoka, M., Miyamoto, T., Kosako, H., Suzuki., J. 2024. In vivo CRISPR screening directly targeting testicular cells. Cell Genomics.
+#    @author:  Noguchi Yuki
+#    @contact: nyuhki21@gmail.com,jsuzuki@icems.kyoto-u.ac.jp
+#
+##REFERENCE
+#1.	Li, W., Xu, H., Xiao, T., Cong, L., Love, M.I., Zhang, F., Irizarry, R.A., Liu, J.S., Brown, M.A., and Liu, X.S. MAGeCK enables robust identification of essential genes from genome-scale CRISPR/Cas9 knockout screens. Genome Biology. 2014; 15: 554. 10.1186/s13059-014-0554-4
+#2. Martin, M. Cutadapt removes adapter sequences from high-throughput sequencing reads. EMBnet.journal. 2011; 17: 10-12. 10.14806/ej.17.1.200
+#3. FastQC: https://www.bioinformatics.babraham.ac.uk/projects/fastqc/
+###########################################################################
 
-##GuideCalling.sh
-#Created by NOGUCHI
-##########################################################################################
-#Defining option
+input_directory=""
+alignment_file=""
+cpu=""
+
 function usage {
     cat <<EOM
 Usage: $(basename "$0") [OPTION]...
     -h          Display help
-    -i VALUE    ID of experiment (ex. "HN00171248" etc...)
-    -f VALUE    Name of Alignment File (ex.Human_GeCKOv2_Library.csv)
-    -c VALUE	Numbers of core
+    -i VALUE    Specify the input directory (ex. "HN00171248" etc...)
+    -f VALUE    Specify the alignment_file (ex.Human_GeCKOv2_Library.csv)
+    -c VALUE	Specify the CPU core
 EOM
     exit 2
 }
 
-#Definition
-while getopts ":i:f:c:h" optKey; do
-    case "$optKey" in
-        i)
-          i=${OPTARG}
-          ;;
-        f)
-          f=${OPTARG}
-          ;;
-		c)
-          c=${OPTARG}
-          ;;
-        '-h'|'--help'|* )
-          usage
-          ;;
-    esac
-done
+function argparse {
+    while getopts ":i:f:c:h" opt; do
+        case "$opt" in
+			i) input_directory="${OPTARG}" ;;
+            f) alignment_file="${OPTARG}" ;;
+			c) cpu="${OPTARG}" ;;
+            h) usage ;;
+            \?) echo "Invalid option: -$OPTARG" >&2; usage ;;
+            :)  echo "Option -$OPTARG requires an argument." >&2; exit 1 ;;
+        esac
+    done
+    shift "$((OPTIND - 1))"
+}
 
-#Environment
-LIST=$(ls ./${i})
-cp ./alignment_files/${f} ./${i}/
-cp matrix.py ./${i}/
-cd ./${i}
+function config {
+	dir=$(pwd)"/guide_caller_v1.0.0"
+	echo "###########################################################"
+	echo "Start guide_caller.."
+	echo " "
+	echo "///Configuration///"
+	echo "version==========:guide_caller_v1.0.0"
+	echo "package directory:${dir}"
+	echo "input directory==:${input_directory}"
+	echo "alignment file===:${alignment_file}"
+	echo "CPU core ========:${cpu}"
+	echo " "
+	echo "###########################################################"
+}
 
+function env_setting {
+	LIST=$(ls ./${input_directory})
+	for SampleID in `echo ${LIST}`;do
+		mkdir ./${input_directory}/${SampleID}/${SampleID}_fastqc
+		mkdir ./${input_directory}/${SampleID}/mageck_result
+	done
+}
 
-#FastQC(1st)>Trimming>FastQC(2nd)>Alignment>Count
-for SampleID in `echo ${LIST}`
-    do
-    mkdir ./${SampleID}/${SampleID}_fastqc \
-          ./${SampleID}/${SampleID}_fastqc/1st \
-          ./${SampleID}/${SampleID}_fastqc/2nd
-    fastqc --nogroup -o ./${SampleID}/${SampleID}_fastqc/1st ./${SampleID}/${SampleID}.fastq
-    FILE=$(cat ./${SampleID}/${SampleID}.fastq \
+function sgRNA_extraction {
+    FILE=$(cat ./${input_directory}/${SampleID}/${SampleID}.fastq \
         | head -n 2 \
         | tail -n 1 \
         | cut -c 1-5
     )
     if [ "${FILE}" = "ATCGA" ]; then
         cutadapt \
-            -j ${c} \
+            -j ${cpu} \
             -u 30 \
-            ./${SampleID}/${SampleID}.fastq > 5-trimmed_${SampleID}.fastq
+            ./${input_directory}/${SampleID}/${SampleID}.fastq > ./${input_directory}/${SampleID}/5-trimmed_${SampleID}.fastq
         cutadapt \
-            -j ${c} \
+            -j ${cpu} \
             -u -1 \
-            5-trimmed_${SampleID}.fastq > trimmed_${SampleID}.fastq
-    
+			./${input_directory}/${SampleID}/5-trimmed_${SampleID}.fastq > ./${input_directory}/${SampleID}/trimmed_${SampleID}.fastq
     elif [ "${FILE}" = "TAGCT" ]; then
         cutadapt \
-            -j ${c} \
+            -j ${cpu} \
             -u 29 \
-            ./${SampleID}/${SampleID}.fastq > 5-trimmed_${SampleID}.fastq
+            ./${input_directory}/${SampleID}/${SampleID}.fastq > ./${input_directory}/${SampleID}/5-trimmed_${SampleID}.fastq
         cutadapt \
-            -j ${c} \
+            -j ${cpu} \
             -u -2 \
-            5-trimmed_${SampleID}.fastq > trimmed_${SampleID}.fastq
-    
+			./${input_directory}/${SampleID}/5-trimmed_${SampleID}.fastq > ./${input_directory}/${SampleID}/trimmed_${SampleID}.fastq
     elif [ "${FILE}" = "CAAGT" ]; then
         cutadapt \
-            -j ${c} \
+            -j ${cpu} \
             -u 28 \
-            ./${SampleID}/${SampleID}.fastq > 5-trimmed_${SampleID}.fastq
+            ./${input_directory}/${SampleID}/${SampleID}.fastq > ./${input_directory}/${SampleID}/5-trimmed_${SampleID}.fastq
         cutadapt \
-            -j ${c} \
+            -j ${cpu} \
             -u -3 \
-            5-trimmed_${SampleID}.fastq > trimmed_${SampleID}.fastq
-    
+			./${input_directory}/${SampleID}/5-trimmed_${SampleID}.fastq > ./${input_directory}/${SampleID}/trimmed_${SampleID}.fastq
     else
         cutadapt \
-            -j ${c} \
+            -j ${cpu} \
             -u 31 \
-            ./${SampleID}/${SampleID}.fastq > trimmed_${SampleID}.fastq
+            ./${input_directory}/${SampleID}/${SampleID}.fastq > ./${input_directory}/${SampleID}/trimmed_${SampleID}.fastq
+	fi
+	fastqc --nogroup -o ./${input_directory}/${SampleID}/${SampleID}_fastqc ./${input_directory}/${SampleID}/trimmed_${SampleID}.fastq
+}
 
-    fi
-    fastqc --nogroup -o ./${SampleID}/${SampleID}_fastqc/2nd trimmed_${SampleID}.fastq
-    mkdir ./${SampleID}/mageck_result
+function sgRNA_count {
     mageck count \
-        -l $f \
+        -l ./alignment_files/${alignment_file} \
         -n ${SampleID} \
-        --fastq trimmed_${SampleID}.fastq
+        --fastq ./${input_directory}/${SampleID}/trimmed_${SampleID}.fastq
+	mv ${SampleID}.* ./${input_directory}/${SampleID}/mageck_result
+	mv *.Rmd *.Rnw *.R ./${input_directory}/${SampleID}/mageck_result
+}
 
-    mv ${SampleID}.log ${SampleID}.*.txt *.R *.Rnw ${SampleID}.count_report.Rmd ./${SampleID}/mageck_result
-    mv trimmed_${SampleID}.fastq 5-trimmed_${SampleID}.fastq ./${SampleID}/
-    
-    python matrix.py ${SampleID}/mageck_result ${SampleID}
-    
+function main {
+    argparse "$@"
+    config
+	env_setting
+	for SampleID in `echo ${LIST}`;do
+		sgRNA_extraction
+		sgRNA_count
+		python ${dir}/matrix_shaper.py -o ${input_directory}/${SampleID}/mageck_result/${SampleID} \
+		< ${input_directory}/${SampleID}/mageck_result/${SampleID}.count.txt
+	done
+}
 
-done
-
+main "$@"
+rm ${dir}/*/trimmed_*.fastq ${dir}/*/5-trimmed_*.fastq
+echo "done."
 exit=0
--- v1.0.0/matrix_shaper.py
+++ v1.0.0/matrix_shaper.py
@@ -1,44 +1,103 @@
-##################
-#matrix.py [1] [2]
-#1: File Directry (HN00171248/F2-1_1/mageck_result)
-#2: File Name (F2-1_1)
-#Created by NOGUCHI,Yuki
-##################
+#!/usr/bin/env python3
+###########################################################################
+#guide_caller_v1.0.0/matrix_shaper.py
+#
+#	 Copyright (c) 2024, Noguchi Yuki (Jun Suzuki lab)
+#	 This software is released under the MIT License, see LICENSE (https://opensource.org/license/mit/).
+#    @citation: Noguchi, Y., Onodera, Y., Maruoka, M., Miyamoto, T., Kosako, H., Suzuki., J. 2024. In vivo CRISPR screening directly targeting testicular cells. Cell Genomics.
+#    @author:  Noguchi Yuki
+#    @contact: nyuhki21@gmail.com,jsuzuki@icems.kyoto-u.ac.jp
+#
+###########################################################################
 
-import pandas as pd
 import numpy as np
+import pandas as pd
 import sys
+import os
+import argparse
+import warnings
+warnings.simplefilter('ignore', FutureWarning)
+pd.options.mode.chained_assignment = None
 
-args = sys.argv
-print("Directry=" + args[1])
-print("File Name=" + args[2])
+def parser_setting():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-i', '--count_file',
+                        action='store',
+                        type=argparse.FileType('r'),
+                        default='-',
+                        help='File path of count file')
+	parser.add_argument('-o', '--output_dir', help='output directry')
+	args = parser.parse_args()
+	return vars(args)
 
-df = pd.read_table(args[1] + "/" + args[2] + ".count.txt", sep = '\t')
-df = df.rename(columns = { df.columns[2]: 'count' })
+def stats(data):
+	df_counted, df_uncounted = data[data['count'] > 0], data[data['count'] == 0]
+	df_counted['mapped'], df_uncounted['mapped'] = 1, 0
+	df_stats = pd.concat([df_counted, df_uncounted]).groupby('Gene').sum().reset_index()
+	df_stats = df_stats.rename(columns={'count':'total_count'})
+	df_stats['Log2(CPM+1)'] = np.log2(1 + 1000000 * df_stats['total_count']/df_stats['total_count'].sum())
+	data['count'],data['sgRNA']= data['count'].astype(str) + ',',data['sgRNA'].astype(str) + ','
+	df_matrix = data.groupby('Gene').sum().reset_index()
+	try: #if sgRNA is aligned to both of poolA and poolB.
+		df_sgRNA_count = data.groupby(by="Gene").sum()["count"].str.split(",",expand=True).drop([6],axis=1).fillna('')
+	except KeyError:
+		try:
+			df_sgRNA_count = data.groupby(by="Gene").sum()["count"].str.split(",",expand=True).drop([4],axis=1).fillna('')
+			print('sgRNA alignment may be done using Pool A library.')
+		except KeyError:
+			df_sgRNA_count = data.groupby(by="Gene").sum()["count"].str.split(",",expand=True).drop([3],axis=1).fillna('')
+			print('sgRNA alignment may be done using Pool B library.')
+	df_sgRNA_count['num_sgRNAs'] = df_sgRNA_count.apply(lambda row: sum(row != ''), axis=1)
+	df_stats = df_stats.merge(df_sgRNA_count.reset_index().loc[:,['Gene','num_sgRNAs']],how="left", on="Gene")
+	df_stats["%mapped"] = 100 * df_stats['mapped'] / df_stats['num_sgRNAs']
+	return df_stats, df_matrix
 
-df_counted = df[df['count'] > 0]
-df_counted['mapped'] = 1
-df_uncounted = df[df['count'] == 0]
-df_uncounted['mapped'] = 0
-df_summary = pd.concat([df_counted, df_uncounted]).groupby('Gene').sum().reset_index()
-df_summary = df_summary.rename(columns = { df.columns[2]: 'total_count' })
+def aggr2matrix(matrix,stats):
+	df_result = stats.loc[:,['Gene', 'Log2(CPM+1)', 'total_count', 'num_sgRNAs', 'mapped', '%mapped']].merge(matrix, on="Gene")
+	try: #if sgRNA is aligned to both of poolA and poolB.
+		df_result[["UID.1(count)","UID.2(count)","UID.3(count)","UID.4(count)","UID.5(count)","UID.6(count)"]] = df_result["count"].str.split(',', expand=True).drop([6],axis=1)
+		df_result[["UID.1","UID.2","UID.3","UID.4","UID.5","UID.6"]] = df_result["sgRNA"].str.split(',', expand=True).drop([6],axis=1)
+		df_result = df_result.loc[:,[
+			'Gene', 'Log2(CPM+1)', 'total_count', 'num_sgRNAs', 'mapped', '%mapped',
+			'UID.1','UID.1(count)',
+			'UID.2','UID.2(count)',
+			'UID.3','UID.3(count)',
+			'UID.4','UID.4(count)',
+			'UID.5','UID.5(count)',
+			'UID.6','UID.6(count)'
+			]]
+	except KeyError:
+		try:
+			df_result[["UID.1(count)","UID.2(count)","UID.3(count)","UID.4(count)"]] = df_result["count"].str.split(',', expand=True).replace('','None').drop([4],axis=1).replace('None','')
+			df_result[["UID.1","UID.2","UID.3","UID.4"]] = df_result["sgRNA"].str.split(',', expand=True).drop([4],axis=1)
+			df_result = df_result.loc[:,[
+				'Gene', 'Log2(CPM+1)', 'total_count', 'num_sgRNAs', 'mapped', '%mapped',
+				'UID.1','UID.1(count)',
+				'UID.2','UID.2(count)',
+				'UID.3','UID.3(count)',
+				'UID.4','UID.4(count)'
+				]]
+		except KeyError:
+			df_result[["UID.1(count)","UID.2(count)","UID.3(count)"]] = df_result["count"].str.split(',', expand=True).replace('','None').drop([3],axis=1).replace('None','')
+			df_result[["UID.1","UID.2","UID.3"]] = df_result["sgRNA"].str.split(',', expand=True).drop([3],axis=1)
+			df_result = df_result.loc[:,[
+				'Gene', 'Log2(CPM+1)', 'total_count', 'num_sgRNAs', 'mapped', '%mapped',
+				'UID.1','UID.1(count)',
+				'UID.2','UID.2(count)',
+				'UID.3','UID.3(count)'
+				]]
+	df_result = df_result.sort_values(by=["total_count"], ascending=False)
+	df_result = df_result.sort_values(by=["%mapped"], ascending=False)
+	return df_result
 
-df['count'] = df['count'].astype(str) + ','
-df['sgRNA'] = df['sgRNA'].astype(str) + ','
-df_matrix = df.groupby('Gene').sum().reset_index()
-
-df_result = df_summary.merge(df_matrix, on="Gene")
-df_result[["UID.1(count)","UID.2(count)","UID.3(count)","UID.4(count)","UID.5(count)","UID.6(count)"]] = df_result["count"].str.split(',', expand=True).drop([6],axis=1)
-df_result[["UID.1","UID.2","UID.3","UID.4","UID.5","UID.6"]] = df_result["sgRNA"].str.split(',', expand=True).drop([6],axis=1)
-df_result = df_result.loc[:,[
-	'Gene', 'total_count', 'mapped',
-	'UID.1','UID.1(count)',
-	'UID.2','UID.2(count)',
-	'UID.3','UID.3(count)',
-	'UID.4','UID.4(count)',
-	'UID.5','UID.5(count)',
-	'UID.6','UID.6(count)'
-	]]
-df_result = df_result.sort_values(by=["total_count"], ascending=False)
-df_result = df_result.sort_values(by=["mapped"], ascending=False)
-df_result.to_csv(args[1] + "/" + args[2] + "_result.csv")
+if __name__ == "__main__":
+	args = parser_setting()
+	"""formatting"""
+	df = pd.read_table(args['count_file'], sep = '\t').rename(columns = {'sample1':'count'})
+	"""stats"""
+	df_stats, df_matrix = stats(data=df)
+	"""finalization"""
+	df_result = aggr2matrix(matrix=df_matrix,stats=df_stats)
+	df_result.to_csv(args["output_dir"] + "_result.csv")
+	print("matrix has been generated!")
+	
